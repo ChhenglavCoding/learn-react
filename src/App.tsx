4ee9692d82823633a68ce6aa501318593ebd8f20
ChhenglavCoding <chhenglav555@gmail.com>
2025-03-05 22:23:12 +0700

    add Testing JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import Greeting from "./components/Greeting";
-import{Greeting1, Greeting2} from "./components/Greeting"
+import{Greeting1, Greeting2} from "./components/Greeting";
+
+import { TestJSX } from "./components/Greeting";
+
+
 
 function App() {
   return (
-    <div>
-      <Greeting/>
-      <Greeting1/>
-      <Greeting2/>
-    </div>
-  )
+
+    <>
+      <TestJSX/>
+      <Greeting1 />
+      <Greeting />
+      <Greeting2 />
+    </>
+    
+  );
 }
 
 export default App
--- src/components/Greeting/index.tsx
+++ src/components/Greeting/index.tsx
@@ -1,24 +1,34 @@
-function Greeting(){
-    return(
-        <h1>hello React default export</h1>
-
-    );
+function Greeting() {
+  return <h1>hello React default export</h1>;
 }
 
-export function Greeting1(){
-    return(
-        <div>
-            <h1>hello React Greeting1</h1>
-        </div>
-    );
+export function Greeting1() {
+  return (
+    <div>
+      <h1>hello React Greeting1</h1>
+    </div>
+  );
 }
 
-export function Greeting2(){
-    return(
-        <div>
-            <h1>hello React Greeting2</h1>
-        </div>
-    );
+export function Greeting2() {
+  return (
+    <div>
+      <h1>hello React Greeting2</h1>
+    </div>
+  );
+}
+
+const name = "Chhenglav";
+const titlegreeting = "Javascript";
+export function TestJSX() {
+  return (
+    <div>
+      <h1>
+        hello {name} {1 + 2}
+      </h1>
+      <h1 title={titlegreeting}>Hello React JS</h1>
+    </div>
+  );
 }
 
 export default Greeting;

d5d5a500517eecf5787e4d0a0b2e8ebf6c8e1073
ChhenglavCoding <chhenglav555@gmail.com>
2025-03-05 22:43:30 +0700

    add object to test JSX component

--- src/components/Greeting/index.tsx
+++ src/components/Greeting/index.tsx
@@ -20,6 +20,19 @@ export function Greeting2() {
 
 const name = "Chhenglav";
 const titlegreeting = "Javascript";
+
+
+interface Myobject {
+    id: number;
+    name: string;
+    age?: number;
+}
+
+const myobject: Myobject = {
+    id: 1,
+    name: "chhenglav",
+    age: 30
+}
 export function TestJSX() {
   return (
     <div>
@@ -27,8 +40,16 @@ export function TestJSX() {
         hello {name} {1 + 2}
       </h1>
       <h1 title={titlegreeting}>Hello React JS</h1>
+
+      <ul>
+        <h1>Object Person:</h1>
+        <li>ID: {myobject.id}</li>
+        <li>Name: {myobject.name.toUpperCase()}</li>
+        <li>Age: {myobject.age}</li>
+      </ul>
     </div>
   );
 }
+
 
 export default Greeting;
